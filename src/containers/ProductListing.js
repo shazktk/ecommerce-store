be903263ae3fb82fe0be26e548d2d1998dbe4c9f
Shahzeb Khan <skhan.bece15@nice.nust.edu.pk>
2023-04-17 22:22:22 +0500

    Ecommerce App - React Redux

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import ProductComponent from './ProductComponent';
+import { fetchProducts } from '../redux/actions/productActions';
+
+const ProductListing = () => {
+
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  },[])
+
+  console.log("Products: ", products)
+
+  return (
+    <div className='ui grid container'>
+      <ProductComponent />
+    </div>
+  )
+}
+
+export default ProductListing
